eeac46a44ed649180b19337e9e12ac7efc94fd16
Nick Hartford <skyler0121@gmail.com>
2020-03-30 14:30:36 -0400

    fixed hybrid display with no results

--- towers/views.py
+++ towers/views.py
@@ -2,32 +2,29 @@ from django.shortcuts import render
 from .models import Towers, Images
 from .forms import SearchForm
 from django.shortcuts import redirect
-# from pages.models import Products
+
 
 # Create your views here.
 ################################# From index page no link parameter ############# Load all towers
 def index(request):
 
-    has_id = False
     
 
     form = SearchForm()
     if request.method == "POST":
         selection = request.POST.get('tower_id')
-        has_id = True
         return redirect('tower_manufacturer', selection)
         
     else:
         towers = Towers.objects.all()
-        main = Images.objects.filter(orientation='main')
 
-    
+
+    title = "ALL NDT"
 
     context = {
         'towers': towers,
         'form': form,
-        'main_img': main,
-        'has_id': has_id,
+        'title': title,
     }
 
     return render(request, 'towers/towers.html', context)
@@ -40,13 +37,19 @@ def tower_manufacturer(request, manufacturer_name):
     form = SearchForm()
 
     towers = Towers.objects.filter(manufacturer=manufacturer_name)
-    main = Images.objects.filter(manufacturer=manufacturer_name, orientation='main')
 
+    if towers:
+        pass
+    else:
+        towers = Towers.objects.filter(manufacturer="ndt")
+
+
+    title = towers[0].get_name()
 
     context = {
         'towers': towers,
         'form': form,
-        'main_img': main,
+        'title': title,
     }
     
 
@@ -59,11 +62,9 @@ def tower_product(request, manufacturer_name, boat_id):
     tower = Towers.objects.filter(slug=boat_id)
     tower_id = tower[0].id
 
-    images = Images.objects.filter(tower_id=tower_id)
     
     context = {
         'tower': tower,
-        "images": images,
     }
 
     return render(request, 'pages/product.html', context)
